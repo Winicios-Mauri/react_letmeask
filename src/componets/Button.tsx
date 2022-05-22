dafe396a33ece07a3a851db48b58cfc2af3655ba
Winicios Meirellis Mauri <winicios.mauri@oncorte.com.br>
2022-05-21 22:23:56 -0300

    :tada letmeask_aula2

--- src/componets/Button.tsx
+++ src/componets/Button.tsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
 
 export function Button(){
-  const [counter, setCounter] = useState(5)
+  const [counter, setCounter] = useState(0)
 
   function increment(){
     setCounter(counter + 1);
-    console.log(counter);
   }
 
   return(
